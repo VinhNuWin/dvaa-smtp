8dd193690858dd75d45b4ecabccc12a711518859
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-09-11 17:12:43 -0700

    user/pass updated

--- utils/contactEmail.js
+++ utils/contactEmail.js
@@ -7,8 +7,93 @@ dotenv.config();
 const nodemailer = require("nodemailer");
 
 const contactEmail = (req, res) => {
-  const output =
-    "Hello! Happy to be connected with you. What project are you wanting to work on? My expertise lies in Website Development, 3D animation, and Virtual Reality. If you are looking to create something entirely new(my favorite), lets get on a call and chat. Contact me at (714) 707-9279 and we can talk more. Cheers! - Vinh Nuwin";
+  const output = `<!doctype html>
+<html lang="en-US"> 
+
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title>Documented Voices Registry</title>
+    <meta name="description" content="Documented Voices Registry Report">
+</head>
+<style>
+    a:hover {text-decoration: underline !important;}
+</style>
+
+<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+        style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+        <tr>
+            <td>
+                <table style="background-color: #f2f3f8; max-width:670px; margin:0 auto;" width="100%" border="0"
+                    align="center" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td style="height:80px;">&nbsp;</td>
+                    </tr>
+                    <!-- Logo -->
+                                <tr>
+                                    <td style="padding:0 15px; text-align:center;">
+                                        <h1 style="color:#1e1e2d; font-weight:200; margin:0;font-size:24px;font-family:'Rubik',sans-serif;">Documented Voices</h1>
+                                        <span style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; 
+                                        width:100px;"></span>
+                                    </td>
+                                </tr>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <!-- Email Content -->
+                    <tr>
+                        <td>
+                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                style="max-width:670px; background:#fff; border-radius:3px;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);padding:0 40px;">
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                                <!-- Title -->
+                                <tr>
+                                    <td style="padding:0 15px; text-align:center;">
+                                        <h1 style="color:#1e1e2d; font-weight:400; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Registry Report</h1>
+                                        <span style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; 
+                                        width:100px;"></span>
+                                    </td>
+                                </tr>
+                                <!-- Details Table -->
+                                <tr>
+                                    <td>
+                                        <table cellpadding="0" cellspacing="0"
+                                            style="width: 100%; border: 1px solid #ededed">
+                                            <tbody>
+                                               <p>Hello! Happy to be connected with you. What project are you wanting to work on? My expertise lies in Website Development, 3D animation, and Virtual Reality. If you are looking to create something entirely new(my favorite), lets get on a call and chat. Contact me at (714) 707-9279 and we can talk more. Cheers!</p>
+                                               <br></br>
+                                               <p>-Vinh Nuwin</p>
+
+
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center;">
+                                <p style="font-size:14px; color:#455056bd; line-height:18px; margin:0 0 0;">&copy; <strong>www.documentedvoices.org</strong></p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>`;
+
+  ("Hello! Happy to be connected with you. What project are you wanting to work on? My expertise lies in Website Development, 3D animation, and Virtual Reality. If you are looking to create something entirely new(my favorite), lets get on a call and chat. Contact me at (714) 707-9279 and we can talk more. Cheers! - Vinh Nuwin");
 
   res.status(200).json();
 
